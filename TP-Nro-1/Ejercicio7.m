4cca6d17df641b110c0a783e801a3db6c98a6e6b
Dbiondi <duiliob@gmail.com>
2014-03-31 17:38:32 -0300

    Create Ejercicio7.m

---
+++ TP-Nro-1/Ejercicio7.m
@@ -0,0 +1,40 @@
+%Inicializacion de parametros
+
+tolerancia = 0.002;
+t_ini = 0;
+t_fin = 1;
+media_ref = 0;
+varianza_ref = 1;
+
+%Prueba 0
+disp('Prueba 0');
+
+Fm = 500;% Cantidad de muestras
+n = 500;% Cantidad de Realizaciones
+f = n;
+c = Fm;
+
+%vector de n señales aleatorias con length(t) muestras
+mats0 = randn(n,Fm);%media 0 varianza 1 por defecto
+
+%Verifico Estacionariedad
+[me,va] = calculos_med_var_est(mats0,c,f);
+
+if (abs(media_ref - me) < tolerancia && abs(varianza_ref - va) < tolerancia)
+	disp('Verifica Estacionariedad');
+	disp('Media Est'), disp(me);
+	disp('Varianza Est'), disp(va);
+
+	%Verifico Ergodicidad
+	[me2,va2] = calculos_med_var_erg(mats0,f,c);
+
+	if (abs(me - me2) < tolerancia && abs(va - va2) < tolerancia )
+		disp('Verifica Ergodicidad');
+		disp('Media Erg'), disp(me2);
+		disp('Varianza Erg'), disp(va2);
+	else
+		disp('No Verifica Ergodicidad');
+    end
+else
+	disp('No Verifica Estacionariedad');
+    end
