375f83556cae65a831174284c2a9e49573e5924c
Dbiondi <duiliob@gmail.com>
2014-03-31 18:32:53 -0300

    Update snr.m

--- TP-Nro-1/snr.m
+++ TP-Nro-1/snr.m
@@ -5,16 +5,16 @@ function [SNR,Ps,Pr] = snr(s,r,t)
 	Ps = 0; %Inicializo la variable en 0
 	for i=1:n
 		Ps = s(i) ^ 2 + Ps;	
-	endfor
+    end
 	Ps = Ps/n;
 
 	%Calculo de potencia (Pr)
 	Pr = 0; %Inicializo la variable en 0
 	for i=1:n
 		Pr = r(i) ^ 2 + Pr;
-	endfor
+    end
 	Pr = Pr/n; 
 
 	%Calculo SNR
 	SNR = 10 * log10 (Ps / Pr);
-endfunction
+    end
