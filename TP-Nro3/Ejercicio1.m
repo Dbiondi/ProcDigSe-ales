e3e1392633b8d3bda271f00842d785b7243a5d9c
Dbiondi <duiliob@gmail.com>
2014-04-21 19:11:11 -0300

    Create Ejercicio1.m

---
+++ TP-Nro3/Ejercicio1.m
@@ -0,0 +1,110 @@
+function TP3Ejercio1()
+T=0.001;
+Fm = 1/T;
+t_ini = 0;
+t_fin = 1;
+t=t_ini:T:t_fin-T;
+
+s = sin(2*pi*10*t) + 4*sin(2*pi*20*t);
+
+subplot(5,2,1);
+plot(s);
+
+%Calculo de la TDF
+fsp=fftp(s);
+subplot(5,2,2);
+plot(fsp);
+
+%Teorema de Parseval
+
+e=energia(s);
+f=energia(fsp);
+f= f * (1/length(t));
+
+
+disp('Si los valores correspondientes a la Energía de la Función y de TDF de la Funcion son iguales, entonces cumple con el Teorema de Parseval');
+
+disp(e);
+
+disp(f);
+
+if e==f
+    disp('Cumple con el Teorema Parseval');
+else
+    disp('No cumple con el Teorema Parseval');
+end
+
+%Primera Modificacion (s1+s2+4)
+s=sin(2*pi*10*t) + 4*sin(2*pi*20*t)+4;
+
+subplot(5,2,3);
+plot(s);
+
+fsp=fftp(s);
+subplot(5,2,4);
+plot(fsp);
+
+%Segunda Modificacion (f1=10 f2=11)
+
+s=sin(2*pi*10*t) + 4*sin(2*pi*11*t)+4;
+
+subplot(5,2,5);
+plot(s);
+
+fsp=fftp(s);
+subplot(5,2,6);
+plot(fsp);
+
+%Tercera Modificacion (f1=10 f2=10.5)
+
+s=sin(2*pi*10*t) + 4*sin(2*pi*10.5*t)+4;
+
+subplot(5,2,7);
+plot(s);
+
+fsp=fftp(s);
+subplot(5,2,8);
+plot(fsp);
+
+%Cuarta Modificacion (Intervalo de tiempo [0...0.72])
+
+t_ini = 0;
+t_fin = 0.72;
+t=t_ini:T:t_fin-T;
+
+s = sin(2*pi*10*t) + 4*sin(2*pi*20*t);
+
+subplot(5,2,9);
+plot(s);
+
+fsp=fftp(s);
+subplot(5,2,10);
+plot(fsp);
+
+end
+
+function [fsp] = fftp(s)
+	%Calcula la transformada y la postprocesa para mostrarla correctamente
+	fs = abs(fft(s));%Fundamental el valor absoluto aca!
+	N = length(fs);
+	vm = floor(N/2);%En el caso que la longitud sea impar???
+
+	fsp = zeros(1,N);%Vector de valores acomodados inicializado
+
+	%Valores negativos
+	for i = 1:vm
+		fsp(i) = fs(vm + i);%desde vm +1 hasta N 
+    end
+	%Valores positivos
+	for i = vm+1:N 
+		fsp(i) = fs(i - vm);%desde 1 hasta vm +1 
+    end
+end
+
+function [e]=energia(s)
+    n=length(s);
+    e=0;
+    for i=1:1:n
+        e=e+abs(s(i))^2;
+    end
+end
